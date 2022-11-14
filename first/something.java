ecb9862a8ce7a432b1aa0bc65ca6abb28b3df91a
daoqingao <daoqingao@gmail.com --git config --global user.email daoqingao@gmail.com>
2022-11-14 12:18:04 -0500

    second diverge

--- first/something.java
+++ first/something.java
@@ -2,6 +2,8 @@ public class something{
     public static void main(String[]args){
         System.out.println("a");
         System.out.println("made new changes again now.");
+        System.out.println("made second diverge");
+
     }
 
 }
